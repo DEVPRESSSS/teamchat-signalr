c004ed628a15490935d39feafdc71c11319a41d2
devpresss xmont <xmontemorjerald@gmail.com>
2026-08-29 21:10:37 +0800

    Added Data Folder with ApplicationDbContex.cs class, installed Microsoft.EfCore and Mapped ApplicationUser and Message Models in DbSet

---
+++ team-chat.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using team_chat.Server.Model;
+
+namespace team_chat.Server.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
