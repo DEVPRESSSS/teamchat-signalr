c25889ed56af8e80af51c73219d0e203a797877c
devpresss xmont <xmontemorjerald@gmail.com>
2026-08-29 20:47:23 +0800

    Added Model folder with ApplicationUser and Message models

---
+++ team-chat.Server/Model/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿namespace team_chat.Server.Model
+{
+    public class ApplicationUser
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = null!;
+        public string? Description { get; set; }
+        public string PasswordHash { get; set; } = null!;
+        public string? ProfilePath { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ team-chat.Server/Model/Message.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace team_chat.Server.Model
+{
+    public class Message
+    {
+        public Guid MessageId { get; set; }
+        public Guid UserId { get; set; }
+        public ApplicationUser? User { get; set; }
+        public string? Content { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
