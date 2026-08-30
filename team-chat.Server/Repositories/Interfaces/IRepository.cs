7f8b976520b1324648f2100884ee7ed955ed1088
devpresss xmont <xmontemorjerald@gmail.com>
2026-08-30 22:46:21 +0800

    Added Repositories folder with Repository.cs Implementation, inside Repositories folder added Interfaces folder with IRepository.cs contract

---
+++ team-chat.Server/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace team_chat.Server.Repositories.Interfaces
+{
+    public interface IRepository<T> where T: class
+    {
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
+        Task AddAsync(T entity);
+        void UpdateAsync(T entity);
+        void Remove(T entity);
+
+    }
+}
---
+++ team-chat.Server/Repositories/Repository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using team_chat.Server.Data;
+using team_chat.Server.Repositories.Interfaces;
+
+namespace team_chat.Server.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> _dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            _dbSet = db.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+          await _dbSet.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return await query.ToListAsync();
+
+
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>>? filter,
+                string? includeProperties = null,bool tracked = false)
+        {
+            IQueryable<T> query;
+
+            if (tracked)
+            {
+                query = _dbSet;
+            }
+            else
+            {
+                query = _dbSet.AsNoTracking();
+            }
+
+            query = query.Where(filter);
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            return await query.FirstOrDefaultAsync();
+
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
