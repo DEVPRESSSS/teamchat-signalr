6f19bcc8ae3281addd5b19e49cf1bf2e4ba1a7a6
devpresss xmont <xmontemorjerald@gmail.com>
2026-09-02 00:13:17 +0800

    Added SendMessageInput.jsx

--- team-chat.client/src/components/SendFooter.jsx
+++ team-chat.client/src/components/SendFooter.jsx
@@ -1,8 +1,9 @@
+import SendMessageInput from "./SendMessageInput";
 
 function SendFooter() {
   return (
-      <div className= "border-t border-gray-200 h-20">
-        
+      <div className="border-t border-gray-200 h-20 p-2">
+          <SendMessageInput placeholder = "Type message here"/>
       </div>
   );
 }
---
+++ team-chat.client/src/components/SendMessageInput.jsx
@@ -0,0 +1,18 @@
+import { SendHorizontal } from 'lucide-react';
+function SendMessageInput({ placeholder }) {
+    return (
+        <div className="flex items-center gap-2 border border-gray-300 px-3 mb-2 py-2 rounded-md w-full
+                         focus-within:border-gray-600 focus-within:ring-2 focus-within:ring-indigo-100
+                         transition-colors">
+           
+            <input
+                className="flex-1 bg-transparent border-none outline-none text-sm placeholder:text-gray-400"
+                placeholder={placeholder}
+            />
+            <div className="shrink-0 text-gray-400">
+                <SendHorizontal className= "text-green-600 cursor-pointer" size={16} />
+            </div>
+        </div>
+    );
+}
+export default SendMessageInput;
