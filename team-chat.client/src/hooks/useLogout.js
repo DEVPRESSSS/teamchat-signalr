7d767c2b10f33329a23f561203ddf29b8abeab88
devpresss xmont <xmontemorjerald@gmail.com>
2026-09-16 20:53:00 +0800

    added sweetalert

--- team-chat.client/src/hooks/useLogout.js
+++ team-chat.client/src/hooks/useLogout.js
@@ -1,13 +1,36 @@
 import { logout } from "../api/authApi";
 import toast from 'react-hot-toast';
-import {useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 export function useLogout() {
     const navigate = useNavigate();
-    const handleConfirmation = async () => {     
-        const response = await logout();
+    const MySwal = withReactContent(Swal)
 
-        toast.success(response.data.message);
-        navigate("/login");
+    const handleConfirmation = async () => {     
+
+        const result = await MySwal.fire({
+            title: "Are you sure you want to logout?",
+            icon: "question",
+            position :"center",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            width: "20rem",
+            confirmButtonText: "Yes",
+            cancelButtonText: "No",
+            customClass: {
+                popup: "swal-popup",
+                title:"swal-title"
+            }
+        });
+        if (result.isConfirmed) {
+            const response = await logout();
+
+            toast.success(response.data.message);
+            navigate("/login");
+        }
+        
     };
     return {handleConfirmation };
 }
